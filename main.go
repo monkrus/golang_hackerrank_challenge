6d7d02f728c175bd1279cff4aa5ac94f1e3002db
Sergei Stadnik <sergeisqa@gmail.com>
2020-03-08 17:01:25 -0700

    Update main.go

--- main.go
+++ main.go
@@ -682,3 +682,27 @@ i     [3] [2] [1]
        j  j+1 j+2
 */
 
+/*
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. 
+Completed code for Person and a declaration for Student are provided for you in the editor.
+Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has 4 parameters:
+A string,firstName .
+A string,lastName .
+An integer,id .
+An integer array (or vector) of test scores, scores.
+A char calculate() method that calculates a Student object's average and returns the grade character
+representative of their calculated average:
+
+
+Letter Average
+ O    90<=a<=100
+ E    80<=a<90
+ A    70<=a<80
+ P    55<=a<70
+ D    40<=a<55
+ T      a<40
+*/
