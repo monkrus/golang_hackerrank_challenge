deb5ff6c5220fd796a818af83471242da5cae2a6
Sergei Stadnik <sergeisqa@gmail.com>
2020-03-10 15:49:11 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-/* 13. 
+/* 14. 
 Complete the Difference class by writing the following:
 
 A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
@@ -31,11 +31,6 @@ d.computeDifference()
 
 print(d.maximumDifference
 
-/* 14.
-Complete the Difference class by writing the following:
 
-A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
-A computeDifference method that finds the maximum absolute difference between any 2 numbers in N and stores it in the maximumDifference instance variable.   
-      */
       
   
