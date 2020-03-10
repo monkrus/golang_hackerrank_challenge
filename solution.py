67a91314285ef5767195c3e100851acf37cd98b7
Sergei Stadnik <sergeisqa@gmail.com>
2020-03-10 14:46:00 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -29,4 +29,13 @@ a = [int(e) for e in input().split(' ')]
 d = Difference(a)
 d.computeDifference()
 
-print(d.maximumDifference)
+print(d.maximumDifference
+
+/* 14.
+Complete the Difference class by writing the following:
+
+A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
+A computeDifference method that finds the maximum absolute difference between any 2 numbers in N and stores it in the maximumDifference instance variable.   
+      */
+      
+  
